6654337f5b605cef5db01e6a632d0255ac1b9d31
bobpepers <bob.pepers@gmail.com>
2019-12-21 12:55:42 +0100

    up

--- src/config.js
+++
@@ -1,8 +0,0 @@
-console.log(process.env.RUNES_EXCHANGE_RATES_TELEGRAM_BOT_TOKEN);
-
-const config = {
-    token: "xxx",
-    maxResults: 30 // 50 is the maximum allowed by Telegram
-};
-
-module.exports = config;
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-cd /home/bago/RunesExchangeRates-Telegram-Bot
-bash
-/usr/bin/screen -X -S rates quit
-/usr/bin/screen -dmS rates
-/usr/bin/screen -S rates -p 0 -X stuff "RUNES_EXCHANGE_RATES_TELEGRAM_BOT=xxx nodemon index.js $(printf \\r)"
