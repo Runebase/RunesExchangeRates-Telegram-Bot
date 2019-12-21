9e2b69d39bfb7e2b85b6e1d179677598a123c814
bobpepers <bob.pepers@gmail.com>
2019-12-21 12:51:07 +0100

    up

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 console.log(process.env.RUNES_EXCHANGE_RATES_TELEGRAM_BOT_TOKEN);
 
 const config = {
-    token: "xxx",
+    token: "t",
     maxResults: 30 // 50 is the maximum allowed by Telegram
 };
 
