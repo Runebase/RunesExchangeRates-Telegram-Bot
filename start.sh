d71e14b31de7f7115783c41eb563bf5e96c7659e
bobpepers <bob.pepers@gmail.com>
2019-12-21 12:47:04 +0100

    remove compromised bot token

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd /home/bago/RunesExchangeRates-Telegram-Bot
 bash
 /usr/bin/screen -X -S rates quit
 /usr/bin/screen -dmS rates
-/usr/bin/screen -S rates -p 0 -X stuff "RUNES_EXCHANGE_RATES_TELEGRAM_BOT=781000602:AAHQaAn84-HM4tGIcOhO4fo1n7rL6nD4kfE nodemon index.js $(printf \\r)"
+/usr/bin/screen -S rates -p 0 -X stuff "RUNES_EXCHANGE_RATES_TELEGRAM_BOT=xxx nodemon index.js $(printf \\r)"
